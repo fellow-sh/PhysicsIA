7b00eeaa860e804996488783e2a8b83057d9bf6f
fellow-sh <jnjoaquin@shaw.ca>
2022-04-14 23:16:53 -0500

    data processing corrected

--- python/plots.py
+++ python/plots.py
@@ -5,7 +5,7 @@ import numpy as np
 import csv
 
 plt.rcParams['text.usetex'] = True
-#plt.rcParams.update({'font.size': 12})
+plt.rcParams.update({'font.size': 11})
 
 path = Path(__file__).parent.parent.resolve() / 'data' / 'image-data2.csv'
 
@@ -42,12 +42,13 @@ ax1.set_xlim(left=0, right=1.6)
 ax1.set_ylim(bottom=0)
 ax1.grid()
 
-get_dx = lambda x, dx : (np.arctan(x + dx) - np.arctan(x - dx)) / 2
+dtan = lambda x : np.tan(np.radians(x))
+get_dy = lambda y, dy, y2 : (dtan(y+dy) - dtan(y-dy))/2 / dtan(y) * y2
 
 X2 = X#np.degrees(np.arctan(X))
 DX2 = DX#[get_dx(x, dx) for x, dx in zip(X, DX)]
-Y2 = mass*9.81*np.tan(np.radians(Y))
-DY2 = DY
+Y2 = mass*9.81*dtan(Y)
+DY2 = get_dy(Y, DY, Y2)
 
 f2 = plt.figure()
 ax2 = f2.add_subplot(111)
@@ -58,9 +59,9 @@ x = np.linspace(np.min(X2)*(0.8), np.max(X2)*1.1, 100)
 
 ax2.plot(x, (res2.slope * x + res2.intercept), 'k')
 
-ax2.set_xlabel(r'Current (\textit{A})')
-ax2.set_ylabel(u'Angle (\u00b0)')
-ax2.set_xlim(left=0)
+ax2.set_xlabel(r'Current (A)')
+ax2.set_ylabel(r'Force (mN)')
+ax2.set_xlim(left=0, right=1.6)
 ax2.set_ylim(bottom=0)
 ax2.grid()
 
@@ -68,7 +69,7 @@ eqstr1 = r'$y=%.2fx + %.2f$' % (res2.slope, res2.intercept)
 eqstr2 = r'$y=%.2fx %.2f$' % (res2.slope, res2.intercept)
 
 textstr = '\n'.join((
-    eqstr1 if res2.intercept > 0 else eqstr2,
+    eqstr1 if res2.intercept >= 0 else eqstr2,
     r'$\rho=%.4f$' % (res2.rvalue, )))
 
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
@@ -76,7 +77,4 @@ props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 ax2.text(0.05, 0.95, textstr, transform=ax2.transAxes, fontsize=12,
         verticalalignment='top', bbox=props)
 
-print(res2.slope)
-print(res2.intercept)
-print(res2.rvalue)
 plt.show()
